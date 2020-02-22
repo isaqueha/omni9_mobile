e242d44fdfd9d70e80944c2a5957c3bd8af1548c
Isaque Azevedo <isaque.azevedo@sap.com>
2020-02-21 21:57:23 -0300

    feat: websockets

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 import Routes from './src/routes';
-import { StyleSheet, Text, View } from 'react-native';
 
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 export default function App() {
   return <Routes />;
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client';
+import { SafeAreaView, ScrollView, StyleSheet, Text, Image, AsyncStorage, Alert } from 'react-native';
 
 import SpotList from '../components/SpotList'
 
@@ -7,6 +8,18 @@ import logo from '../../assets/logo.png';
 
 export default function List() {
 	const [techs, setTechs] = useState(['a','b', 'c']);
+
+	userEffect(() => {
+		AsyncStorage.getItem('user').then(user_id => {
+			const socket = socketio('http://i4-ead.anonymous.mobile.exp.direct:3333', {
+				query: { user_id }
+			})
+
+			socket.io('booking_response', booking => {
+				Alert.alert(`Your booking in ${booking.spot.company} on ${booking.date} was ${booking.approved ? 'APPROVED' : 'REJECTED'}`)
+			})
+		})
+	}, [])
 
 	useEffect(() => {
 		AsyncStorage.getItem('techs').then(storageTechs => {
